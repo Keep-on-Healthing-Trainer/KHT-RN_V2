168f07168d540763c07a849a91bc94bf67f4eb8c
downpour07 <choiheeyoung10@gmail.com>
2024-07-11 15:56:02 +0900

    📝 :: Login page 설정

---
+++ kht/src/screens/Auth/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, Image, StyleSheet } from "react-native";
+import { color } from "../../styles/theme";
+import constants from "../../styles/constants";
+
+const LoginPage = ({navigation}) => {
+
+    return(
+        <Text>LOGINPAGE</Text>
+    );
+}
+
+export default LoginPage;
