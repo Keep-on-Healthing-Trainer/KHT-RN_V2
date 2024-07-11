b601124f00b9b77edcc92ef149695327506ec406
downpour07 <choiheeyoung10@gmail.com>
2024-07-11 15:57:04 +0900

    📝 :: Search page 설정

---
+++ kht/src/screens/Search/Result/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, Image, StyleSheet } from "react-native";
+import { color } from "../../../styles/theme";
+import constants from "../../../styles/constants";
+
+const ResultPage = ({navigation}) => {
+
+    return(
+        <Text>RESULTPAGE</Text>
+    );
+}
+
+export default ResultPage;
