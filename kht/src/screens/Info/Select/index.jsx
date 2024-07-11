78452eb0db979b6815bffd84cd61e307f788e3d5
downpour07 <choiheeyoung10@gmail.com>
2024-07-11 15:56:24 +0900

    📝 :: Info page 설정

---
+++ kht/src/screens/Info/Select/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, Image, StyleSheet } from "react-native";
+import { color } from "../../../styles/theme";
+import constants from "../../../styles/constants";
+
+const SelectPage = ({navigation}) => {
+
+    return(
+        <Text>SELECTPAGE</Text>
+    );
+}
+
+export default SelectPage;
