dcd9546bfc1d9fe0ef8a335922992d995f90d64b
downpour07 <choiheeyoung10@gmail.com>
2024-07-11 15:57:29 +0900

    📝 :: Signup page 설정

---
+++ kht/src/screens/User/Count/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, Image, StyleSheet } from "react-native";
+import { color } from "../../../styles/theme";
+import constants from "../../../styles/constants";
+
+const CountPage = ({navigation}) => {
+
+    return(
+        <Text>COUNTPAGE</Text>
+    );
+}
+
+export default CountPage;
---
+++ kht/src/screens/User/Intent/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, Image, StyleSheet } from "react-native";
+import { color } from "../../../styles/theme";
+import constants from "../../../styles/constants";
+
+const IntentPage = ({navigation}) => {
+
+    return(
+        <Text>INTENTPAGE</Text>
+    );
+}
+
+export default IntentPage;
