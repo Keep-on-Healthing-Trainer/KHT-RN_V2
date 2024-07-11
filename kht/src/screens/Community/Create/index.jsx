189a567595206f35f57eb10e93dcafaa244a9532
downpour07 <choiheeyoung10@gmail.com>
2024-07-11 15:56:12 +0900

    📝 :: Community page 설정

---
+++ kht/src/screens/Community/Create/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, Image, StyleSheet } from "react-native";
+import { color } from "../../../styles/theme";
+import constants from "../../../styles/constants";
+
+const CreatePage = ({navigation}) => {
+
+    return(
+        <Text>CREATEPAGE</Text>
+    );
+}
+
+export default CreatePage;
---
+++ kht/src/screens/Community/View/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, Image, StyleSheet } from "react-native";
+import { color } from "../../../styles/theme";
+import constants from "../../../styles/constants";
+
+const ViewPage = ({navigation}) => {
+
+    return(
+        <Text>VIEWPAGE</Text>
+    );
+}
+
+export default ViewPage;
