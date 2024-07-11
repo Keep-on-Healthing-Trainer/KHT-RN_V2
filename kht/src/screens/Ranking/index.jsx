a635fab613eae6d05354dede9baebc64a0ef2831
downpour07 <choiheeyoung10@gmail.com>
2024-07-11 15:56:53 +0900

    📝 :: Ranking page 설정

---
+++ kht/src/screens/Ranking/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, Image, StyleSheet } from "react-native";
+import { color } from "../../styles/theme";
+import constants from "../../styles/constants";
+
+const RankingPage = ({navigation}) => {
+
+    return(
+        <Text>RANKINGPAGE</Text>
+    );
+}
+
+export default RankingPage;
