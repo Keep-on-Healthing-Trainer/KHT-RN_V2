b3ae774fad99a1e15460b198e70866dfcf8119fe
downpour07 <choiheeyoung10@gmail.com>
2024-07-13 22:09:33 +0900

    📑 :: Button components 추가

---
+++ kht/src/components/Button/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { color } from "../../styles/theme";
+import constants from "../../styles/constants";
+
+const Button = ( props ) => {
+
+    return(
+        <TouchableOpacity style={Styles.container} onPress={props.onPress}>
+            <Text style={Styles.font} >
+                {props.innerText}
+            </Text>
+        </TouchableOpacity>
+    );
+}
+
+const Styles = StyleSheet.create({
+    container: {
+        width: constants.width/1.2,
+        height: constants.height/20,
+        backgroundColor: color.Blue[10],
+        borderRadius: 10,
+        paddingLeft: 10,
+        paddingRight: 10,
+        fontSize: 14,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    font: {
+        color: color.White,
+        fontSize: 14
+    }
+})
+
+export default Button;
