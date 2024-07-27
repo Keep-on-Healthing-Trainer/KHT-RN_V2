d67b64ffae2936b5adf3e237d069263a7606c59f
downpour07 <choiheeyoung10@gmail.com>
2024-07-27 22:05:46 +0900

    ⚡️ :: My page Chart 기능

---
+++ kht/src/screens/Info/Main/LineChart/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { View } from "react-native";
+import LineChart from "./Chart";
+import useXY from './useXY';
+
+const Chart = (props) => {
+  const data = props.data;
+  const [arr] = useXY(data);
+
+  return (
+    <View>
+      <LineChart points={arr} data={data} />
+    </View>
+  );
+}
+
+export default Chart;
