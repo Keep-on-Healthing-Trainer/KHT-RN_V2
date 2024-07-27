f7372c0ad3eaa331cc8d2a462b579603ca6103d2
downpour07 <choiheeyoung10@gmail.com>
2024-07-27 22:04:44 +0900

    📑 :: icon 추가

---
+++ kht/src/assets/icon/Back.jsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+import Svg, { Path } from "react-native-svg"
+
+function BackButton(props) {
+  return (
+    <Svg
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <Path
+        d="M15 19l-7-7 7-7"
+        stroke="#000"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </Svg>
+  )
+}
+
+export default BackButton;
