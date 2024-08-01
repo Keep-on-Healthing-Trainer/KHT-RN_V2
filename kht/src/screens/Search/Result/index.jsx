c745220338da13cb30c4f62b2866e133667bc6e5
downpour07 <choiheeyoung10@gmail.com>
2024-08-01 18:49:21 +0900

    📝 :: Result page 퍼블리싱

--- kht/src/screens/Search/Result/index.jsx
+++ kht/src/screens/Search/Result/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Image, FlatList, StyleSheet } from "react-native";
+import { Text, View, Image, ScrollView, StyleSheet, TouchableOpacity } from "react-native";
 import { color } from "../../../styles/theme";
 import constants from "../../../styles/constants";
 
@@ -8,65 +8,99 @@ import BackButton from "../../../assets/icon/Back";
 
 const ResultPage = ({navigation}) => {
 
-    const data = {
-        "resultData": [
-            {
-                "exerciseName": "캡틴스 체어 레그 레이즈",
-                "title": "복근, 허벅지",
-                "text": "기본 레그 레이즈는 누워서 했다면 캡틴스 체어 레그 레이즈는 선채로  다리를 드는 운동이에요. 서서 다리를 올리는 운동이다 보니 코어뿐만 아니라 전신 근육이 쓰이게 돼요!"
-            },
-            {
-                "exerciseName": "캡틴스 체어 레그 레이즈",
-                "title": "복근, 허벅지",
-                "text": "기본 레그 레이즈는 누워서 했다면 캡틴스 체어 레그 레이즈는 선채로  다리를 드는 운동이에요. 서서 다리를 올리는 운동이다 보니 코어뿐만 아니라 전신 근육이 쓰이게 돼요!"
-            },
-            {
-                "exerciseName": "캡틴스 체어 레그 레이즈",
-                "title": "복근, 허벅지",
-                "text": "기본 레그 레이즈는 누워서 했다면 캡틴스 체어 레그 레이즈는 선채로  다리를 드는 운동이에요. 서서 다리를 올리는 운동이다 보니 코어뿐만 아니라 전신 근육이 쓰이게 돼요!"
-            },
-            {
-                "exerciseName": "캡틴스 체어 레그 레이즈",
-                "title": "복근, 허벅지",
-                "text": "기본 레그 레이즈는 누워서 했다면 캡틴스 체어 레그 레이즈는 선채로  다리를 드는 운동이에요. 서서 다리를 올리는 운동이다 보니 코어뿐만 아니라 전신 근육이 쓰이게 돼요!"
-            },
-            {
-                "exerciseName": "캡틴스 체어 레그 레이즈",
-                "title": "복근, 허벅지",
-                "text": "기본 레그 레이즈는 누워서 했다면 캡틴스 체어 레그 레이즈는 선채로  다리를 드는 운동이에요. 서서 다리를 올리는 운동이다 보니 코어뿐만 아니라 전신 근육이 쓰이게 돼요!"
-            },
-            {
-                "exerciseName": "캡틴스 체어 레그 레이즈",
-                "title": "복근, 허벅지",
-                "text": "기본 레그 레이즈는 누워서 했다면 캡틴스 체어 레그 레이즈는 선채로  다리를 드는 운동이에요. 서서 다리를 올리는 운동이다 보니 코어뿐만 아니라 전신 근육이 쓰이게 돼요!"
-            },
-            {
-                "exerciseName": "캡틴스 체어 레그 레이즈",
-                "title": "복근, 허벅지",
-                "text": "기본 레그 레이즈는 누워서 했다면 캡틴스 체어 레그 레이즈는 선채로  다리를 드는 운동이에요. 서서 다리를 올리는 운동이다 보니 코어뿐만 아니라 전신 근육이 쓰이게 돼요!"
-            },
-        ]
-      }
+    const onClickData = () => {
+        navigation.navigate("DataPage", { screen: 'DataPage' });
+    }
 
     return(
         <View style={Styles.container}>
             <View style={Styles.back}>
-                <BackButton />
+                <View style={Styles.button}>
+                    <BackButton />
+                </View>
                 <Search />
             </View>
-            <FlatList
-                data={data.resultData}
-                renderItem={(item) => {
-                    return (
-                    <View style={Styles.box}>
-                        <View style={Styles.img}></View>
-                        <Text style={Styles.boldText}>{item.exerciseName}</Text>
-                        <Text style={Styles.text}>{item.title}</Text>
-                        <Text style={Styles.text}>{item.text}</Text>
+            <View style={Styles.select}>
+                <TouchableOpacity style={Styles.focusSelect}>
+                    <Text style={Styles.focusSelectText}>전체</Text> 
+                </TouchableOpacity>
+                <TouchableOpacity style={Styles.noFocusSelect}>
+                    <Text style={Styles.noFocusSelectText}>허리</Text> 
+                </TouchableOpacity>
+                <TouchableOpacity style={Styles.noFocusSelect}>
+                    <Text style={Styles.noFocusSelectText}>복근</Text> 
+                </TouchableOpacity>
+                <TouchableOpacity style={Styles.noFocusSelect}>
+                    <Text style={Styles.noFocusSelectText}>팔</Text> 
+                </TouchableOpacity>
+                <TouchableOpacity style={Styles.noFocusSelect}>
+                    <Text style={Styles.noFocusSelectText}>허벅지</Text> 
+                </TouchableOpacity>
+                <TouchableOpacity style={Styles.noFocusSelect}>
+                    <Text style={Styles.noFocusSelectText}>종아리</Text> 
+                </TouchableOpacity>
+                <TouchableOpacity style={Styles.noFocusSelect}>
+                    <Text style={Styles.noFocusSelectText}>등</Text> 
+                </TouchableOpacity>
+            </View>
+            <ScrollView>
+                <TouchableOpacity style={Styles.box} onPress={() => onClickData()}>
+                    <View style={Styles.img}></View>
+                    <View style={Styles.right}> 
+                        <Text style={Styles.boldText}>캡틴스 체어 레그 레이즈</Text>
+                        <Text style={Styles.text}>복근, 허벅지</Text>
+                        <Text style={Styles.text}>기본 레그 레이즈는 누워서 했다면 캡틴스 체어 레그 레이즈는 선채로  다리를 드는 운동이에요. 서서 다리를 올리는 운동이다 보니 코어뿐만 아니라 전신 근육이 쓰이게 돼요!</Text>
                     </View>
-                    );
-                }}
-            />
+                </TouchableOpacity>
+                <TouchableOpacity style={Styles.box}>
+                    <View style={Styles.img}></View>
+                    <View style={Styles.right}> 
+                        <Text style={Styles.boldText}>캡틴스 체어 레그 레이즈</Text>
+                        <Text style={Styles.text}>복근, 허벅지</Text>
+                        <Text style={Styles.text}>기본 레그 레이즈는 누워서 했다면 캡틴스 체어 레그 레이즈는 선채로  다리를 드는 운동이에요. 서서 다리를 올리는 운동이다 보니 코어뿐만 아니라 전신 근육이 쓰이게 돼요!</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity style={Styles.box}>
+                    <View style={Styles.img}></View>
+                    <View style={Styles.right}> 
+                        <Text style={Styles.boldText}>캡틴스 체어 레그 레이즈</Text>
+                        <Text style={Styles.text}>복근, 허벅지</Text>
+                        <Text style={Styles.text}>기본 레그 레이즈는 누워서 했다면 캡틴스 체어 레그 레이즈는 선채로  다리를 드는 운동이에요. 서서 다리를 올리는 운동이다 보니 코어뿐만 아니라 전신 근육이 쓰이게 돼요!</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity style={Styles.box}>
+                    <View style={Styles.img}></View>
+                    <View style={Styles.right}> 
+                        <Text style={Styles.boldText}>캡틴스 체어 레그 레이즈</Text>
+                        <Text style={Styles.text}>복근, 허벅지</Text>
+                        <Text style={Styles.text}>기본 레그 레이즈는 누워서 했다면 캡틴스 체어 레그 레이즈는 선채로  다리를 드는 운동이에요. 서서 다리를 올리는 운동이다 보니 코어뿐만 아니라 전신 근육이 쓰이게 돼요!</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity style={Styles.box}>
+                    <View style={Styles.img}></View>
+                    <View style={Styles.right}> 
+                        <Text style={Styles.boldText}>캡틴스 체어 레그 레이즈</Text>
+                        <Text style={Styles.text}>복근, 허벅지</Text>
+                        <Text style={Styles.text}>기본 레그 레이즈는 누워서 했다면 캡틴스 체어 레그 레이즈는 선채로  다리를 드는 운동이에요. 서서 다리를 올리는 운동이다 보니 코어뿐만 아니라 전신 근육이 쓰이게 돼요!</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity style={Styles.box}>
+                    <View style={Styles.img}></View>
+                    <View style={Styles.right}> 
+                        <Text style={Styles.boldText}>캡틴스 체어 레그 레이즈</Text>
+                        <Text style={Styles.text}>복근, 허벅지</Text>
+                        <Text style={Styles.text}>기본 레그 레이즈는 누워서 했다면 캡틴스 체어 레그 레이즈는 선채로  다리를 드는 운동이에요. 서서 다리를 올리는 운동이다 보니 코어뿐만 아니라 전신 근육이 쓰이게 돼요!</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity style={Styles.box}>
+                    <View style={Styles.img}></View>
+                    <View style={Styles.right}> 
+                        <Text style={Styles.boldText}>캡틴스 체어 레그 레이즈</Text>
+                        <Text style={Styles.text}>복근, 허벅지</Text>
+                        <Text style={Styles.text}>기본 레그 레이즈는 누워서 했다면 캡틴스 체어 레그 레이즈는 선채로  다리를 드는 운동이에요. 서서 다리를 올리는 운동이다 보니 코어뿐만 아니라 전신 근육이 쓰이게 돼요!</Text>
+                    </View>
+                </TouchableOpacity>
+            </ScrollView>
         </View>
     );
 }
@@ -84,29 +118,84 @@ const Styles = StyleSheet.create({
         width: constants.width,
         height: constants.height/9,
         backgroundColor: color.White,
-        alignItems: 'flex-start',
-        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+        justifyContent: 'flex-start',
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 20,
+        padding: 10,
+    },
+    button: {
+        height: constants.height/20,
+        justifyContent: 'center',
     },
     box: {
         width: constants.width,
         height: constants.height/8,
-        backgroundColor: color.Blue[10],
+        borderBottomWidth: 1,
+        borderBottomColor: color.Gray[1],
+        padding: 10,
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 20,
     },
     img: {
-        width: 50,
-        height: 70,
+        width: 70,
+        height: 100,
+        backgroundColor: color.Black,
     },
     boldText: {
-        fontSize: 14,
+        fontSize: 15,
         fontWeight: 'bold',
         color: color.Black,
     },
+    right: {
+        display: 'flex',
+        rowGap: 2,
+    },
     text: {
-        fontSize: 10,
+        fontSize: 12,
         fontWeight: '500',
         color: color.Gray[4],
         width: constants.width/1.5,
     },
+    select: {
+        width: constants.width,
+        height: constants.height/32,
+        borderBottomWidth: 1,
+        borderBottomColor: color.Gray[1],
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+    },
+    focusSelect: {
+        paddingLeft: 2,
+        paddingRight: 2,
+        height: constants.height/32,
+        display: 'flex',
+        alignItems: 'center',
+        borderBottomColor: color.Black,
+        borderBottomWidth: 1,
+    },
+    focusSelectText: {
+        fontSize: 15,
+        color: color.Black,
+        fontWeight: '500',
+    },
+    noFocusSelect: {
+        paddingLeft: 2,
+        paddingRight: 2,
+        height: constants.height/32,
+        display: 'flex',
+        alignItems: 'center',
+    },
+    noFocusSelectText: {
+        fontSize: 15,
+        color: color.Gray[4],
+        fontWeight: '500',
+    }
 });
 
 export default ResultPage;
