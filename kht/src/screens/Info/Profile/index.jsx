bb5cb4a3c70efe847a49d3376242b106c544e3e5
downpour07 <choiheeyoung10@gmail.com>
2024-08-03 23:44:49 +0900

    ⚡️ :: profile 기능 구현

--- kht/src/screens/Info/Profile/index.jsx
+++ kht/src/screens/Info/Profile/index.jsx
@@ -1,28 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View, Image, StyleSheet, TouchableOpacity } from "react-native";
+import * as DocumentPicker from 'expo-document-picker';
+import axios from "axios";
+
 import { color } from "../../../styles/theme";
 import constants from "../../../styles/constants";
 
 import BackHeader from "../components/Header";
 
 const ProfilePage = ({navigation}) => {
+    const [imageUrl, setImageUrl] = useState();
+    const formData = new FormData();
 
     const onClickBack = () => {
         navigation.navigate("SelectPage", { screen: 'SelectPage' });
     }
 
-    return(
+    const uploadImage = async () => {
+        try {
+            const result = await DocumentPicker.getDocumentAsync({
+                type: 'image/*',
+            });
+            console.log(result);
+
+            //if (result.type === 'success') {
+                setImageUrl(result.assets[0].uri);
+                formData.append("image", result);
+                formData.append("userId", '');
+                API();
+            //}
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    const API = () => {
+        const token = '';
+        axios
+          .post(
+            `{API_URL}/user/modifyProfile`,
+            formData,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          )
+          .then((res) => {
+            console.log('성공');
+          })
+          .catch((err) => {
+            console.error(err);
+            console.log('실패');
+          });
+      };
+
+    return (
         <View style={Styles.container}>
             <BackHeader data="프로필 사진" onPress={() => onClickBack()} />
             <View style={Styles.profileContainer}>
-                <View style={Styles.profile}></View>
+                <Image style={Styles.profile} source={imageUrl ? {uri: imageUrl} : undefined} />
                 <View style={Styles.textContainer}>
                     <Text style={Styles.text}>프로필 이미지를 설정하지 않을</Text>
                     <Text style={Styles.text}>경우에는 기본 프로필로 설정됩니다.</Text>
                 </View>
             </View>
             <View style={Styles.buttonContainer}>
-                <TouchableOpacity style={Styles.button}>
+                <TouchableOpacity style={Styles.button} onPress={() => uploadImage()}>
                     <Text style={Styles.buttonText}>수정하기</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={Styles.button}>
@@ -35,12 +79,12 @@ const ProfilePage = ({navigation}) => {
 
 const Styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: color.White,
-      width: constants.width,
-      height: constants.height,
-      alignItems: 'center',
-      rowGap: 70,
+        flex: 1,
+        backgroundColor: color.White,
+        width: constants.width,
+        height: constants.height,
+        alignItems: 'center',
+        rowGap: 70,
     },
     profileContainer: {
         width: constants.width,
