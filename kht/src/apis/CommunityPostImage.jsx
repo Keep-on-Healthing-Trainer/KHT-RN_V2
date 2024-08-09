58bb736ea2d99b10a59af7b1ab19572829afbbd7
downpour07 <choiheeyoung10@gmail.com>
2024-08-09 11:20:19 +0900

    ⚡️ :: CommunityPostImage API

---
+++ kht/src/apis/CommunityPostImage.jsx
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import { Alert } from 'react-native';
+import * as SecureStore from "expo-secure-store";
+
+const onCommunityPostImage = async ( data, id ) => {
+
+    const result = await SecureStore.getItemAsync('token');
+    const token = result && JSON.parse(result);
+
+    try {
+        const response = await axios.post(`${process.env.API_KEY}/post/postImage/${id}`,
+            data,
+            {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": 'multipart/form-data',
+              },
+            }
+          );
+
+        if(response.status == 200) {
+            console.log("이미지 업로드에 성공했습니다.");
+            return true;
+        }
+    } catch (error) {
+        if (error.response) {
+            if (error.response.status === 403) {
+              console.log('image : 유저를 찾을 수 없습니다');
+              Alert.alert('유저를 찾을 수 없습니다.');
+            } else {
+              console.log('image : 이미지 업로드 오류');
+              Alert.alert('이미지 업로드 오류입니다.');
+            }
+        } else {
+            console.log('image : 네트워크 오류');
+            Alert.alert('네트워크 오류입니다.');
+        }
+        return false;
+    }
+};
+
+export default onCommunityPostImage;
