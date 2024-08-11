14ced135e05f04fa9ec2997ce22e7b098560a10a
downpour07 <choiheeyoung10@gmail.com>
2024-08-11 23:19:18 +0900

    📝 :: Loading

--- kht/src/screens/Loading/index.jsx
+++ kht/src/screens/Loading/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Text, View, Image, StyleSheet } from "react-native";
 import { color } from "../../styles/theme";
 import constants from "../../styles/constants";
