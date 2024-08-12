6e20845e401bdb42e5456ad00b7dc9e345d786e8
downpour07 <choiheeyoung10@gmail.com>
2024-08-12 20:12:10 +0900

    ⚡️ :: GetCommunityData API

---
+++ kht/src/apis/CommunityGet.jsx
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { Alert } from 'react-native';
+import { getStorage, setStorage, removeStorage } from "../utils/Storage";
+
+const onCommunityGet = async ( id ) => {
+
+    const result = await getStorage('token');
+    const token = result && JSON.parse(result);
+    
+    try {
+        const response = await axios.get(`${process.env.API_KEY}/post/${id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        if(response.status == 200) {
+            console.log("게시물 상세보기 가져오기에 성공하였습니다.");
+            return response.data;
+        }
+    } catch (error) {
+        if (error.response) {
+            if (error.response.status === 404) {
+              console.log('community get : 잘못된 요청입니다.');
+              Alert.alert('잘못된 요청입니다.');
+            } else {
+              console.log('community get : 상세보기 오류');
+              Alert.alert('상세보기 오류입니다.');
+            }
+        } else {
+            console.log('community get : 네트워크 오류');
+            Alert.alert('네트워크 오류입니다.');
+        }
+        return false;
+    }
+};
+
+export default onCommunityGet;
